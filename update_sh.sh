e158c9124dae27dc93bc86b27f02945354df192a
Collect <hui99995@gmail.com>
2023-02-05 21:18:57 +0800

    23.02.05

--- update_sh.sh
+++ update_sh.sh
@@ -5,7 +5,7 @@ cd /usr/sju/sh
 rm -rf 	Re_Linux_Air* bk_Air* *html
 url="https://raw.githubusercontent.com/hui99995/sh/main/"	
 wget $url"Re_Linux_Air.sh" && chmod +x Re_Linux_Air.sh
-wget $url"bk_Air.sh" && chmod +x bk_Air.sh
+#wget $url"bk_Air.sh" && chmod +x bk_Air.sh
 
 rm -rf 	*html *html*
 chmod u+x *  /usr/sju/Temp/*
